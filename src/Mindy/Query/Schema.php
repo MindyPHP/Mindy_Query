e6aef929e52cafaaf1271513763d0258a73d2318
Falaleev Maxim <max@studio107.ru>
2015-02-06 16:01:47 +0300

    Up

--- src/Mindy/Query/Schema.php
+++ src/Mindy/Query/Schema.php
@@ -65,7 +65,7 @@ abstract class Schema
      * If left part is found in DB error message exception class from the right part is used.
      */
     public $exceptionMap = [
-        'SQLSTATE[23' => 'yii\db\IntegrityException',
+        'SQLSTATE[23' => 'Mindy\Query\IntegrityException',
     ];
     /**
      * @var array list of ALL table names in the database
